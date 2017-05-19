88f8e55ff5483bf422f7a4011fd9394c8b378dbc
Tomas Kriska <tomas@kriska.net>
2017-05-20 00:23:14 +0200

    modified base.html

--- app/views.py
+++ app/views.py
@@ -16,3 +16,15 @@ def index():
             }
             ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/signin', methods=['GET','POST'])
+def signin():
+    return render_template('signin.html', title='Sign in')
+    
+@app.route('/signup', methods=['GET','POST'])
+def signun():
+    return render_template('signup.html', title='Sign up')
+   
+@app.route('/account/<id>')
+def account(id):
+    return render_template('account.html', title='Account',id=id)
